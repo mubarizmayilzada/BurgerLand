2d0dc68173a37ece0269213d87715d015fb20236
mubarizmayilzade <mubarizbm@code.edu.az>
2021-12-20 00:55:52 +0400

    home page 70%

---
+++ BurgerLand-FrontEnd/assets/js/filter.js
@@ -0,0 +1,41 @@
+(function() {
+    let field = document.querySelector('.product-cards');
+    let li = Array.from(field.children);
+    
+    function FilterProduct() {
+        let indicator = document.querySelector('.filter-buttons').children;
+    
+        this.run = function() {
+            for(let i=0; i<indicator.length; i++)
+            {
+                indicator[i].onclick = function () {
+                    console.log("marz");
+                    for(let x=0; x<indicator.length; x++)
+                    {
+                        indicator[x].classList.remove('active');
+                    }
+                    this.classList.add('active');
+                    const displayItems = this.getAttribute('data-filter');
+    
+                    for(let z=0; z<li.length; z++)
+                    {
+                        li[z].style.transform = "scale(0)";
+                        setTimeout(()=>{
+                            li[z].style.display = "none";
+                        }, 500);
+    
+                        if ((li[z].getAttribute('data-category') == displayItems) || displayItems == "all")
+                         {
+                             li[z].style.transform = "scale(1)";
+                             setTimeout(()=>{
+                                li[z].style.display = "block";
+                            }, 500);
+                         }
+                    }
+                };
+            }
+        }
+    }
+
+		new FilterProduct().run();
+})();
